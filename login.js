39a762785f4760ea635ece65adb6beaee34b0d6d
ANA JÚLIA SILVA MACEDO <24122496@CFP127ALUNO.LOCAL>
2025-03-18 17:22:20 -0300

    salvando aula 2

---
+++ login.js
@@ -0,0 +1,37 @@
+'use strict'
+const email = document.getElementById('inputusuarioemail')
+const senha = document.getElementById('inputsenha')
+
+
+
+ async function validarLogin(){
+    if (email == "" || email ==  undefined || senha == "" | senha == undefined){
+        alert("Por favor, verifique as credenciais")
+    }else{
+        const url = "https://back-spider.vercel.app/login"
+        const dados = {
+            email : email,
+            senha: senha
+        }
+        const options = {
+            method: POST,
+            headers: {
+                "content/type": "application/JSON"
+            },
+            body: JSON.stringify(dados)
+
+        }
+
+
+        const response = await fetch (url, options)
+
+        if(response == 200){
+
+        }else{}
+
+    }
+}
+
+
+
+document.getElementById('entrar').addEventListener('click', validarLogin)
